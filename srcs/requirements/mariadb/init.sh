b5351ec7315a0632a312106110f9388119101236
Zakaria Laksyar <zalaksya@e1r9p9.1337.ma>
2026-05-12 14:04:08 +0100

    9rib

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
+set -e
+
+rm -rf /var/lib/mysql/*
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-fi
+    echo "db initialized ........................................."
+    nohup mysqld --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0 > /dev/null 2>&1 &
 
-mysqld --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0 &
-sleep 5
+    sleep 5
 
-mysql -u root << EOF
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
+    mysql -u root << EOF
+    CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+    CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+    GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+    FLUSH PRIVILEGES;
 EOF
 
-wait
+
+fi
+
+
+until false; do
+    echo "ayayayayay"
+    sleep 5
+done

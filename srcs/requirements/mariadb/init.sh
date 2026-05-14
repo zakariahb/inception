be4c1123752a2f4b137b0f3f9fcff75f20c1abdd
ZAKARIA <client@zakaria.local>
2026-05-14 23:53:06 +0100

    done

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-
+rm -rf /var/lib/mysql
 set -e
-
-# mysql -e "CREATE DATABASE wordpress;"
-# mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'password';"
-# mysql -e "SHOW databases;"
-
-while true
-do
-  echo "Running... "
-  sleep 1
-done
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mariadb-install-db --user=mysql \
+       --datadir=/var/lib/mysql
+       echo "Initializing DB..."
+    mysqld_safe &
+    echo "Initializing DB..."
+    sleep 5
+    echo "Initializing DB..."
+    mysql -e "CREATE DATABASE wordpress;"
+    mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'password';"
+    mysql -e "SHOW databases;"
+    sleep 5
+    mysqladmin shutdown
+fi
+exec mysqld --user=mysql

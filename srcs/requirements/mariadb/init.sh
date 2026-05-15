2d491e93a9e6921b76f800f4daa0ab21eb72b7e0
Zakaria Laksyar <zalaksya@e1r9p9.1337.ma>
2026-05-15 15:55:53 +0100

    9rib

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-rm -rf /var/lib/mysql
 set -e
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mariadb-install-db --user=mysql \
-       --datadir=/var/lib/mysql
-       echo "Initializing DB..."
-    mysqld_safe &
-    echo "Initializing DB..."
-    sleep 5
-    echo "Initializing DB..."
-    mysql -e "CREATE DATABASE wordpress;"
-    mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'password';"
-    mysql -e "SHOW databases;"
-    sleep 5
-    mysqladmin shutdown
-fi
-exec mysqld --user=mysql
+
+rm -rf /var/lib/mysql
+
+INIT_DB=/tmp/init.sqlz
+
+cat > "$INIT_DB" <<EOF
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER 'wordpress'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
+mysqld --user=mysql --init-file="$INIT_DB"

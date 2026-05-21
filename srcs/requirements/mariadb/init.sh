53a1d20fab4f8c801884e6dbabac82d4d69c3d8a
ZAKARIA <client@zakaria.local>
2026-05-21 23:33:02 +0100

    done

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
-<<<<<<< HEAD
 
-rm -rf /tmp/init.sql
-rm -rf /var/lib/mysql/*
-=======
 rm -rf /var/lib/mysql/*
 rm -rf /tmp/init.sql/tmp/init.sql
->>>>>>> f7a35e6ea2f4d5e88401c44eb88eaa70310c7a7b
 INIT_DB=/tmp/init.sql
 
 cat > "$INIT_DB" <<EOF

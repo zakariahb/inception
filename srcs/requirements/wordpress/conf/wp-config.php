e9706f29c7693a332e77ea6b179d34be5b3214c3
Zakaria Laksyar <zalaksya@e1r9p9.1337.ma>
2026-05-22 12:04:33 +0100

    9rib

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,5 @@
+<?php
+define( 'DB_NAME', getenv(MYSQL_DATABASE));
+define( 'DB_USER', getenv(MYSQL_USER) );
+define( 'DB_PASSWORD', getenv(MYSQL_PASSWORD) );
+define( 'DB_HOST', getenv(MYSQL_HOST) );
